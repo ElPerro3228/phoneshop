603f85deedcfcb9694f0f74b95170f98aca758d4
ElPerro3228 <51485312+ElPerro3228@users.noreply.github.com>
2020-06-10 20:48:09 +0300

    updated DefaultCartPriceCalculationService

--- core/src/main/java/com/es/core/services/DefaultCartPriceCalculationService.java
+++ core/src/main/java/com/es/core/services/DefaultCartPriceCalculationService.java
@@ -2,12 +2,14 @@ package com.es.core.services;
 
 import com.es.core.cart.Cart;
 import com.es.core.cart.CartItem;
+import com.es.core.model.phone.Phone;
 import com.es.core.model.phone.PhoneDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DefaultCartPriceCalculationService implements CartPriceCalculationService{
@@ -18,10 +20,14 @@ public class DefaultCartPriceCalculationService implements CartPriceCalculationS
     @Override
     public BigDecimal calculateCartPrice(Cart cart) {
         List<CartItem> cartItems = cart.getCartItems();
-        double price = cartItems.stream()
-                .filter(cartItem -> phoneDao.get(cartItem.getPhoneId()).isPresent())
-                .mapToDouble(cartItem -> phoneDao.get(cartItem.getPhoneId()).get().getPrice().doubleValue() * cartItem.getQuantity())
-                .sum();
-        return new BigDecimal(price);
+        BigDecimal cartPrice = new BigDecimal("0");
+        for (CartItem cartItem : cartItems) {
+            Optional<Phone> phone = phoneDao.get(cartItem.getPhoneId());
+            BigDecimal quantity = new BigDecimal(cartItem.getQuantity());
+            if (phone.isPresent()) {
+                cartPrice = cartPrice.add(phone.get().getPrice().multiply(quantity));
+            }
+        }
+        return cartPrice;
     }
 }
